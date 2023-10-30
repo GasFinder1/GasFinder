13ae531bb67b7c8fffbbaaf22f94288528bf676b
AdryanSales <adryan.sales21@gmail.com>
2023-10-29 21:30:08 -0300

    Ajusta propriedades da tela de Cálculo de etanol e gasolina

--- frontend/src/pages/CalcRendimento/CalcRendimento.jsx
+++ frontend/src/pages/CalcRendimento/CalcRendimento.jsx
@@ -1,142 +1,164 @@
-import { useState } from 'react'
-import { toast } from "react-toastify";
-import styles from './CalcRendimento.module.css'
-import NavBar from '../../components/NavBar/NavBar'
+  import { useState } from 'react'
+  import { toast } from "react-toastify";
+  import styles from './CalcRendimento.module.css'
+  import NavBar from '../../components/NavBar/NavBar'
 
-const CalcRendimento = () => {
-  const [ etanol, setEtanol ] = useState("");
-  const [ gasolina, setGasolina ] = useState("");
-  const [ result, setResult ] = useState("");
-  
-  function calcPrice(e) {
-    e.preventDefault();
-    try {
-      let price = {
-        etanol,
-        gasolina 
-      };
-      
-      price[etanol] = parseFloat(e.target.value);
-      setEtanol(etanol)
-      price[gasolina] = parseFloat(e.target.value);
-      setGasolina(gasolina)
-      
-      let value = etanol / gasolina;
-      // console.log(value)
-      
-      // Informa erro caso os valores inseridos sejam 0 
-      if ((gasolina == 0) && (etanol == 0) ) {
-        toast.error('Valor da gasolina e etanol inválidos.', {
-          position: "bottom-center",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-          });
-          return;
-      } else if (etanol == 0 ) {
-        toast.error('Valor do etanol inválido.', {
-          position: "bottom-center",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-          });
-          return;
-      } else if (gasolina == 0) {
-        toast.error('Valor da gasolina inválido.', {
-          position: "bottom-center",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-          });
-          return;
+  const CalcRendimento = () => {
+    const [ etanol, setEtanol ] = useState("");
+    const [ gasolina, setGasolina ] = useState("");
+    const [ resultado, setResultado] = useState('Aqui aparece o resultado');
+
+    const handleInputChange = (e, setFuncao) => {
+      e.preventDefault();
+      try {
+        let valorDigitado = e.target.value;
+        let valor = valorDigitado.replace(',','.').split();
+        if (isNaN(valor) === true) {
+          valorDigitado = '';
+        }
+        setFuncao(valorDigitado.replace(',','.'));
+      } catch(err) {
+        console.log(err)
       }
-
-      // cálculo: se valor de (etanol + 42,66%) for menor que o valor da gasolina 
-      if (((etanol*1.4267) < gasolina))  {
-        toast.info('Vale mais a pena abastecer com Etanol!', {
-          position: "bottom-center",
-          autoClose: 8000,
+    };
+    const calcPrice = (e) => {
+      e.preventDefault();
+      try {
+        const alc = parseFloat(etanol);
+        const gas = parseFloat(gasolina);
+        // Informa erro caso os valores inseridos sejam 0 
+        if ((gas <= 0) && (alc <= 0) ) {
+          toast.error('Valor da gasolina e etanol inválidos. Informe um valor maior que zero.', {
+            position: "bottom-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+            setEtanol('')
+            setGasolina('')
+            return;
+        } else if (alc <= 0) {
+          toast.error('Valor do etanol inválido. Informe um valor maior que zero.', {
+            position: "bottom-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+            setEtanol('')
+            setGasolina('')
+            return;
+        } else if (gas <= 0) {
+          toast.error('Valor da gasolina inválido. Informe um valor maior que zero.', {
+            position: "bottom-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+            setEtanol('')
+            setGasolina('')
+            return;
+        }
+        // cálculo: se valor de (etanol + 42,66%) for menor que o valor da gasolina 
+        if ((alc <= (gas*0.7)))  {
+          console.log(gas*0.7)
+          setResultado('Vale mais a pena abastecer com Etanol!')
+        } else if (alc > (gas*0.7)) {
+          console.log(gas*0.7)
+          setResultado('Vale mais a pena abastecer com Gasolina!');
+        } else {
+          toast.error('Valores inválidos, por favor informe o preço adequadamente: "R$0.00".', {
+            position: "bottom-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+            setEtanol('')
+            setGasolina('')
+            return;
+        }
+      setEtanol(etanol)
+      setGasolina(gasolina)
+      } catch(err) {
+        toast.error(`Problema técnico. ${err}`, {
+          position: "top-right",
+          autoClose: 5000,
           hideProgressBar: false,
           closeOnClick: true,
           pauseOnHover: true,
           draggable: true,
-          progress: false,
-          theme: "light",
-          });
-      } else {
-        console.log(etanol*1.4267)
-        toast.info('Vale mais a pena abastecer com Gasolina!', {
-          position: "bottom-center",
-          autoClose: 8000,
-          hideProgressBar: true,
-          closeOnClick: true,
-          pauseOnHover: false,
-          draggable: false,
           progress: undefined,
           theme: "light",
           });
-      } 
-      
-      
-    } catch(err) {
-      toast.error(`Problema técnico. ${err}`, {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-        });
+          setEtanol('')
+          setGasolina('')
+      };
     };
-  };
+    return (
+      <div>
+        <NavBar/>
+        <section className={styles.container}>
+          <form className={styles.formCalc} onSubmit={calcPrice}>
+            <h2>Cálculo de <br/>Etanol x Gasolina </h2>
+            <div className={styles.boxCalc}>
+              <div className={styles.inputsCalc}>
+                <h3>Etanol (preço por litro)</h3>
+                <div className={styles.inputsPreco}>
+                  <p>R$</p>
+                  <input
+                    type="text"
+                    maxLength={4}
+                    placeholder="0,00"
+                    inputMode="numeric"
+                    className={styles.inputGas}
+                    required
+                    onChange={(e) => handleInputChange(e, setEtanol)}
+                    value={etanol}
+                  />
+                </div>
+                <br />
+                <h3>Gasolina (preço por litro)</h3>
+                <div className={styles.inputsPreco}>
+                  <p>R$</p>
+                  <input
+                    type="text"
+                    maxLength={4} 
+                    inputMode="numeric"
+                    placeholder="0,00"
+                    className={styles.inputEtanol}
+                    required
+                    onChange={(e) => handleInputChange(e, setGasolina)}
+                    value={gasolina}  
+                  />
+                </div>
+                <br />
+                <input type="submit" value="Calcular" className={styles.btnCalc}/>
+              </div>
+              <div className={styles.explicacao}>
+                <h3>Como funciona o cálculo?</h3>
+                <p>O cálculo fornece uma noção aproximada de qual combustível vale mais a pena, com base no princícpio de que o etanol só vale a pena enquanto o seu preço for equivalente a <strong>até 70% do valor da gasolina</strong>.</p>
+                <p className={styles.resultado}>{resultado}</p>
+              </div>
+            </div>
+          </form>
+        </section>
+      </div>
+    )
+  }
 
-  return (
-    <div>
-      <NavBar/>
-      <section className={styles.container}>
-      <form className={styles.formCalc} onSubmit={calcPrice}>
-        <h2>Cálculo de Rendimento</h2>
-        <div className={styles.inputsCalc}>
-          <h3>Etanol (preço por litro)</h3>
-          <input
-            type="text"
-            className={styles.inputGas}
-            placeholder="R$0,00"
-            required
-            onChange={(e) => setEtanol(e.target.value)}
-            value={etanol}
-          />
-          <br />
-          <h3>Gasolina (preço por litro)</h3>
-          <input
-            type="text"
-            className={styles.inputEtanol}
-            placeholder="R$0,00"
-            required
-            onChange={(e) => setGasolina(e.target.value)}
-            value={gasolina}
-          />
-          <br />
-        </div>
-        <input type="submit" value="Calcular" className={styles.btnCalc}/>
-      </form>
-    </section>
-    </div>
-  )
-}
-
-export default CalcRendimento;
+  export default CalcRendimento;
