2542738488fed3d661d329ce74a7c8be66b7826b
Pedro2737 <ph030293@gmail.com>
2023-12-07 16:04:50 -0300

    mudado a forma de pegar os dados dos postos que forma inseridos por todos usuários

--- backend/BackEnd_API/src/services/userFuelValuesServices.js
+++ backend/BackEnd_API/src/services/userFuelValuesServices.js
@@ -95,13 +95,33 @@ async function getFuelValuesByPlaceID(place_ID) {
     const conn = await database.getConnection();
     if (conn === null) return { error: "não foi possível se comunicar com o banco de dados" }
     try {
+        let data = { gasolina: false, gasolinaA: false, etanol: false,
+        dieselS10: false, dieselS500: false, gnv: false};
         let sql = "SELECT * FROM tbl_localizacao_posto WHERE place_ID = ?"
         let [rows] = await conn.execute(sql, [place_ID],);
         if (rows.length === 0) throw new Error("não foi possível pegar um posto na seguinte place_ID: " + place_ID);
-        sql = "SELECT * FROM tbl_colaborativa WHERE fk_id_tlp = ?";
-        [rows] = await conn.execute(sql, [place_ID]);
-        if (rows.length < 1) throw new Error("não foi encontrado nenhum valor inserido por esse usuário nesse posto");
-        return rows;
+        sql = `SELECT valor_inserido, COUNT(*) AS contador
+        FROM tbl_colaborativa
+        WHERE dt_atualização >= DATE_SUB(CURDATE(), INTERVAL 2 MONTH)
+        AND fk_id_tlp = ?
+        AND fk_id_combustivel = ?
+        GROUP BY valor_inserido
+        ORDER BY contador DESC, valor_inserido DESC
+        LIMIT 10;`;
+        const id_tlp = rows[0].id_tlp;
+        let field = null;
+        for (let i = 1; i <= 6; i++) {
+            [rows] = await conn.execute(sql, [id_tlp, i]);
+            console.log(rows)
+            if (i == 1) field = "gasolina"
+            else if (i == 2) field = "gasolinaA"
+            else if (i == 3) field = "etanol"
+            else if (i == 4) field = "dieselS10"
+            else if (i == 5) field = "dieselS500"
+            else if (i == 6) field = "gnv"
+            if (rows.length > 0) data[field] = rows;
+        }
+        return data;
     }
     catch (err) {
         //LOG_HERE
